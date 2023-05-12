aeb021013911c73a80909cd51c8812f4c605775e
Jahid <jahidhasan.developer@gmail.com>
2023-05-12 21:43:10 +0700

    update responsive

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,19 +1,26 @@
+"use client"
 import Image from 'next/image';
 import Link from 'next/link';
+import { useState } from 'react';
 
 const Header = () => {
+  const [leftMenu,setLeftMenu] = useState(false);
+
   return (
     <div className="block absolute w-full top-0 z-index-2">
       <header className=" py-5 container flex justify-between items-center">
-        <nav className="nav">
+        <nav className={`nav nav-1 ${leftMenu ? 'active':''}`}>
           <ul className="flex gap-5 items-center">
-            <li>
+            <li className='flex items-center justify-between'>
               <Link
                 className="text-xl uppercase  font-normal text-[#DACD57]"
                 href={'/'}
               >
                 Home
               </Link>
+              <div onClick={() => setLeftMenu(false)} className="close hidden">
+                <Image src={'/close.png'} width={26} height={100} />
+              </div>
             </li>
             <li>
               <Link className="text-xl uppercase  font-normal " href={'/about'}>
@@ -22,10 +29,13 @@ const Header = () => {
             </li>
           </ul>
         </nav>
-        <div>
+        <div className='logo-wrap'>
+          <div className="menu" onClick={() => setLeftMenu(true)}>
+            <Image width={26} height={100} src={'/menu.png'}/>
+          </div>
           <Image alt='logo' src={'/logo.png'} width={300} height={38} />
         </div>
-        <div className="nav">
+        <div className={`nav nav-2 ${leftMenu ? 'active':''}`}>
           <ul className="flex gap-5 items-center">
             <li>
               <Link scroll={false} className="text-xl uppercase  font-normal " href="/#contact">
